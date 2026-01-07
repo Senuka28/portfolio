512fa36bb5d31e03a6aa206fab31fbae54bbebbb
Senuka Abeysinghe <senukaabeysinghe@Mac.lan>
2026-01-07 14:40:03 -0500

    initial formatting

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,29 +7,47 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div>
+    <header className="navbar">
+      <nav>
+        <a href="#home">Home</a>{" | "}
+        <a href="#experience">Experience</a>{" | "}
+        <a href="#projects">Projects</a>{" | "}
+        <a href="#skills">Skills</a>{" | "}
+        <a href="#contact">Contact</a>{" | "}
+      </nav>
+    </header>
+
+    <main>
+        <div className='container'>
+          <section id="home">
+            <h1>Hi, I'm Senuka</h1>
+            <p>This is the home section.</p>
+          </section>
+
+          <section id="experience">
+            <h2>Experience</h2>
+            <p>This is the experience section.</p>
+          </section>
+
+          <section id="projects">
+            <h2>Projects</h2>
+            <p>This is the projects section.</p>
+          </section>
+
+          <section id="skills">
+            <h2>Skills</h2>
+            <p>This is the skills section.</p>
+          </section>
+
+          <section id="contact">
+            <h2>Contact</h2>
+            <p>This is the contact section.</p>
+          </section>
+        </div>
+      </main>
+  </div>
+  );
 }
 
 export default App
